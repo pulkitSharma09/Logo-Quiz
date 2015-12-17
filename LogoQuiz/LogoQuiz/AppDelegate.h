e34dff61a2307aeb9013c0f5addba2e740379ffc
Pulkit <pulkit.sharma0909@yahoo.in>
2015-12-17 18:16:33 +0530

    First Commit ! Project created :)

---
+++ LogoQuiz/LogoQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogoQuiz
+//
+//  Created by Pulkit on 17/12/15.
+//  Copyright © 2015 Pulkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogoQuiz/LogoQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogoQuiz
+//
+//  Created by Pulkit on 17/12/15.
+//  Copyright © 2015 Pulkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
